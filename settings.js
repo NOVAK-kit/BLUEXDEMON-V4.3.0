7443992d6b1ea3c6cd11ca5c5ca0f882bbad61ba
NOVAK-kit <boluwatifeferanmi434@gmail.com>
2025-01-12 19:02:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2349073521743"
 
 
 
 
 //DO NOT EDIT OR MODD
-global.botNumber = pairingNumber
+global.2349073521743 = pairingNumber
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
